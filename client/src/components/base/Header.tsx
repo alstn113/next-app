13bbdd831205074f93375957993a94eca9a49c8c
neko113 <alstn113@gmail.com>
2022-08-07 20:34:21 +0900

    feat : add post delete modal

--- client/src/components/base/Header.tsx
+++ client/src/components/base/Header.tsx
@@ -121,8 +121,8 @@ const Container = styled.header`
 `;
 
 const Title = styled.div`
-  font-size: 18px;
-  font-weight: 600;
+  font-size: 30px;
+  font-weight: 900;
 `;
 
 export default Header;
--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,8 +1,13 @@
+import Button from '@/components/common/Button/Button';
 import Card from '@/components/common/Card/Card';
+import Modal from '@/components/common/Modal/Modal';
 import PostAPI from '@/libs/api/post';
+import useDisclosure from '@/libs/hooks/common/useDisclosure';
+import useDeletePost from '@/libs/hooks/queries/post/useDeletePost';
 import useGetPost from '@/libs/hooks/queries/post/useGetPost';
 import useGetME from '@/libs/hooks/queries/user/useGetMe';
 import formatDate from '@/libs/utils/formatDate';
+import { flexCenter } from '@/styles/shared';
 import styled from '@emotion/styled';
 import { DehydratedState, QueryClient, dehydrate } from '@tanstack/react-query';
 import {
@@ -16,26 +21,45 @@ import { useRouter } from 'next/router';
 const PostDetail: NextPage = () => {
   const router = useRouter();
   const id = router.query.id as string;
+  const { mutate } = useDeletePost(id, {
+    onSuccess: () => {
+      router.push('/');
+    },
+  });
 
   const { data } = useGetPost(id);
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Container>
-      <Card variant="bordered">Title : {data?.title}</Card>
-      <Card variant="flat">Body : {data?.body}</Card>
-      <Card variant="shadow">CreatedAt : {formatDate(data?.createdAt)}</Card>
-      <Card variant="flat">
-        {data?.comments?.map((comment) => (
-          <div key={comment.id}>{comment.text}</div>
-        ))}
-      </Card>
-    </Container>
+    <>
+      <Container>
+        <Card variant="bordered">Title : {data?.title}</Card>
+        <Card variant="flat">Body : {data?.body}</Card>
+        <Card variant="shadow">CreatedAt : {formatDate(data?.createdAt)}</Card>
+        <Button shadow size="lg" onClick={onOpen}>
+          삭제
+        </Button>
+        <Card variant="flat">
+          {data?.comments?.map((comment) => (
+            <div key={comment.id}>{comment.text}</div>
+          ))}
+        </Card>
+      </Container>
+      <Modal
+        title="지우시겠습니까?"
+        message="주의!!"
+        visible={isOpen}
+        onCancel={onClose}
+        onConfirm={mutate}
+      />
+    </>
   );
 };
 
 const Container = styled.div`
+  ${flexCenter}
+  flex-direction: column;
   margin-top: 2rem;
-  div + div {
+  * + * {
     margin-top: 1rem;
   }
 `;
