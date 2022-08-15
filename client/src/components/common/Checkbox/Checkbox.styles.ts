114aaa60a3355f7a40810162e868ddc75d93efbc
neko113 <alstn113@gmail.com>
2022-08-15 19:30:05 +0900

    feat : add Checkbox styles

--- client/src/components/common/Checkbox/Checkbox.styles.ts
+++ client/src/components/common/Checkbox/Checkbox.styles.ts
@@ -1,3 +1,29 @@
 import styled from '@emotion/styled';
 
-export const Root = styled.div``;
+export const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const CheckboxText = styled.span`
+  font-size: 1.2rem;
+  font-weight: 900;
+  user-select: none;
+`;
+
+export const CheckboxPoint = styled.span``;
+
+export const CheckboxInput = styled.input`
+  display: none;
+
+  & ~ ${CheckboxPoint} {
+  }
+
+  &:checked {
+    & ~ ${CheckboxPoint} {
+      &::after {
+      }
+    }
+  }
+`;
--- client/src/components/common/Checkbox/Checkbox.tsx
+++ client/src/components/common/Checkbox/Checkbox.tsx
@@ -1,7 +1,22 @@
-export interface Props {}
+import * as S from './Checkbox.styles';
+import { NormalColorType } from '@/lib/styles/palette';
+import { forwardRef, InputHTMLAttributes } from 'react';
 
-const Checkbox = ({}: Props) => {
-  return <div>Checkbox</div>;
-};
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  labelText?: string;
+  color?: NormalColorType;
+}
+const Checkbox = forwardRef<HTMLInputElement, Props>(function Checkbox(
+  { labelText = '', color = 'primary', ...options },
+  ref,
+) {
+  return (
+    <S.CheckboxLabel>
+      <S.CheckboxInput type="checkbox" ref={ref} color={color} {...options} />
+      <S.CheckboxPoint color={color} />
+      <S.CheckboxText>{labelText}</S.CheckboxText>
+    </S.CheckboxLabel>
+  );
+});
 
 export default Checkbox;
