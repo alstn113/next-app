0cdc4ea1b880458ba383b39339abab6953566c6f
neko113 <alstn113@gmail.com>
2022-08-21 10:50:49 +0900

    chore : ErrorMessage style 변경

--- client/src/components/common/@Components/ErrorMessage/ErrorMessage.styles.ts
+++ client/src/components/common/@Components/ErrorMessage/ErrorMessage.styles.ts
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 
 export const Root = styled.p`
   color: #f31260;
-  font-size: 1rem;
-  font-weight: 500;
+  font-size: 0.8rem;
+  font-weight: 700;
   margin-top: 0.5rem;
   padding: 0 8px;
 `;
