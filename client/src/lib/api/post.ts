a328f4e43b6943662ce420fd34cfb48111f0ab8f
neko113 <alstn113@gmail.com>
2022-08-22 12:28:15 +0900

    chore : 잡다한 것

--- client/src/lib/api/post.ts
+++ client/src/lib/api/post.ts
@@ -12,8 +12,8 @@ const PostAPI = {
     const { data } = await apiClient.get(`/post/search?keyword=${keyword}`);
     return data;
   },
-  getPostBySlug: async (postSlug: string) => {
-    const { data } = await apiClient.get(`/post/${postSlug}`);
+  getPostBySlug: async (slug: string) => {
+    const { data } = await apiClient.get(`/post/${slug}`);
     return data;
   },
   createPost: async (params: CreatePostParams) => {
--- client/src/lib/error.ts
+++ client/src/lib/error.ts
@@ -1,3 +1,4 @@
 import { AxiosError } from 'axios';
 
+//임시 에러
 export interface CustomAxiosError extends AxiosError<{ message: string; statusCode: number }> {}
