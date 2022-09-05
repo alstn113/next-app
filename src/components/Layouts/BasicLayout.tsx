ecdaeec8e20f2133b98655c9d8fef63102a8eb9c
neko113 <alstn113@gmail.com>
2022-09-05 12:32:43 +0900

    feat : BasicLayout 생성

--- src/components/Layouts/BasicLayout.tsx
+++ src/components/Layouts/BasicLayout.tsx
@@ -1,20 +1,50 @@
+import useGoBack from '@/hooks/useGoBack';
+import { flexCenter } from '@/lib/styles/shared';
 import styled from '@emotion/styled';
+import React from 'react';
+import Header from '../base/Header';
+import { ArrowLeft } from '../vectors';
 
 interface Props {
+  title?: React.ReactNode;
   children: React.ReactNode;
   hasBackButton?: boolean;
+  headerRight?: React.ReactNode;
 }
 
-const BasicLayout = ({ children }: Props) => {
+const BasicLayout = ({
+  title,
+  children,
+  hasBackButton,
+  headerRight,
+}: Props) => {
+  const onGoBack = useGoBack();
   return (
-    <Container>
-      <Header />
-      {children}
-    </Container>
+    <>
+      <Header
+        title={title}
+        headerLeft={
+          hasBackButton ? <ArrowLeft onClick={onGoBack} /> : undefined
+        }
+        headerRight={headerRight}
+      />
+      <Container>
+        <Content>{children}</Content>
+      </Container>
+    </>
   );
 };
 
-const Container = styled.div``;
-const Header = styled.header``;
+const Container = styled.div`
+  margin: 80px 0;
+  height: 100%;
+  ${flexCenter};
+`;
+
+const Content = styled.div`
+  ${flexCenter};
+  height: 100%;
+  flex-direction: column;
+`;
 
 export default BasicLayout;
