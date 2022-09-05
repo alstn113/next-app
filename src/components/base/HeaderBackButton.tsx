902c401c4cafe9addb147bfbfc1afb64f5b025bd
neko113 <alstn113@gmail.com>
2022-09-05 12:41:58 +0900

    feat : HeaderBackButton 완성

--- src/components/base/HeaderBackButton.tsx
+++ src/components/base/HeaderBackButton.tsx
@@ -1,9 +1,23 @@
+import { flexCenter } from '@/lib/styles/shared';
+import styled from '@emotion/styled';
+import { ArrowLeft } from '../vectors';
+
 interface Props {
   onClick: () => void;
 }
 
 const HeaderBackButton = ({ onClick }: Props) => {
-  return <div>HeaderBackButton</div>;
+  return (
+    <ArrowLeftButton onClick={onClick}>
+      <ArrowLeft />
+    </ArrowLeftButton>
+  );
 };
+
+const ArrowLeftButton = styled.button`
+  ${flexCenter}
+  padding: 8px;
+  margin-left: -8px;
+`;
 
 export default HeaderBackButton;
