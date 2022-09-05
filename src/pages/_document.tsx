47a91055adde685854e59119c03ea3e5bfa04fc7
neko113 <alstn113@gmail.com>
2022-09-05 15:57:59 +0900

    feat : _document.tsx에 modal 태그 생성

---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <div id="modal"></div>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
