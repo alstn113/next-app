8058e182e921f47323558fe6fa0110e435dcc40b
neko113 <alstn113@gmail.com>
2022-09-13 00:06:27 +0900

    refactor : header 수정

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 import { useRef, useState, useEffect, useCallback } from 'react';
 import getScrollTop from '@/lib/utils/getScrollTop';
 import zIndexes from '@/lib/styles/zIndexes';
-import palette from '@/lib/styles/palette';
 
 interface Props {
   title?: React.ReactNode;
@@ -12,65 +11,8 @@ interface Props {
 }
 
 const Header = ({ title = 'ABYSS', headerLeft, headerRight }: Props) => {
-  const blockRef = useRef<HTMLDivElement>(null);
-  const [marginTop, setMarginTop] = useState(0);
-
-  useEffect(() => {
-    if (!blockRef.current) return;
-    setMarginTop(0);
-  }, []);
-
-  const height = 60;
-  const prevScrollTop = useRef(0);
-  const direction = useRef<'UP' | 'DOWN'>('DOWN');
-  const transitionPoint = useRef(height);
-
-  const handleScroll = useCallback(() => {
-    const scrollTop = getScrollTop();
-    const nextDirection = prevScrollTop.current > scrollTop ? 'UP' : 'DOWN';
-
-    if (
-      direction.current === 'DOWN' &&
-      nextDirection === 'UP' &&
-      transitionPoint.current - scrollTop < 0
-    ) {
-      transitionPoint.current = scrollTop;
-    } else if (
-      direction.current === 'UP' &&
-      nextDirection === 'DOWN' &&
-      scrollTop - transitionPoint.current < -1 * height
-    ) {
-      transitionPoint.current = scrollTop + height;
-    }
-
-    if (direction.current === 'DOWN' && nextDirection === 'DOWN') {
-      setMarginTop(
-        Math.max(-60, -1 * height + transitionPoint.current - scrollTop),
-      );
-    } else {
-      setMarginTop(
-        Math.min(0, -1 * height + transitionPoint.current - scrollTop),
-      );
-    }
-
-    direction.current = nextDirection;
-    prevScrollTop.current = scrollTop;
-  }, [height]);
-
-  useEffect(() => {
-    document.addEventListener('scroll', handleScroll);
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
-    };
-  }, [handleScroll]);
-
   return (
-    <Container
-      style={{
-        marginTop: marginTop,
-      }}
-      ref={blockRef}
-    >
+    <Container>
       {headerLeft && <HeaderSide position="left">{headerLeft}</HeaderSide>}
       <Title>{title}</Title>
       {headerRight && <HeaderSide position="right">{headerRight}</HeaderSide>}
